a3c4b5970bf27300e5a9ef07596908d1dd706548
DavidDgxd <20251028@s.ubaguio.edu>
2026-09-24 09:59:01 +0800

    feat: replaced services  with Injectable, then added Hoenn Leaders Info

--- src/app/services/hoenn.ts
+++ src/app/services/hoenn.ts
@@ -1,12 +1,65 @@
-import { Injectable, Service, signal } from '@angular/core';
-
-@Service()
+import { Injectable, signal } from '@angular/core';
+import { HoennLeader } from '../gym-leader.model';
+@Injectable({
+    providedIn: 'root'
+})
 export class HoennServices {
-    private registry = signal([
+    private registry = signal<HoennLeader[]>([
         {
-            name: 'Ash Ketchum',
-            team: ['Pikachu', 'Bulbasaur', 'Charizard'],
-            items: ['Master Ball', 'Potion', 'Revive'],
+            name: 'Roxanne',
+            town: 'Rustboro City',
+            specialty: 'Rock',
+            badge: 'Stone Badge',
+            pokemonTeam: ['Geodude', 'Onix', 'Nosepass']
+        },
+        {
+            name: 'Brawly',
+            town: 'Dewford Town',
+            specialty: 'Fighting',
+            badge: 'Knuckle Badge',
+            pokemonTeam: ['Makuhita', 'Meditite', 'Machop']
+        },
+        {
+            name: 'Wattson',
+            town: 'Mauville City',
+            specialty: 'Electric',
+            badge: 'Dynamo Badge',
+            pokemonTeam: ['Magnemite', 'Voltorb', 'Electrike']
+        },
+        {
+            name: 'Flannery',
+            town: 'Lavaridge Town',
+            specialty: 'Fire',
+            badge: 'Heat Badge',
+            pokemonTeam: ['Numel', 'Slugma', 'Torkoal']
+        },
+        {
+            name: 'Norman',
+            town: 'Petalburg City',
+            specialty: 'Normal',
+            badge: 'Balance Badge',
+            pokemonTeam: ['Slaking', 'Vigoroth',  'Linoone']
+        },
+        {
+            name: 'Winona',
+            town: 'Fortree City',
+            specialty: 'Flying',
+            badge: 'Feather Badge',
+            pokemonTeam: ['Swellow', 'Pelipper', 'Skarmory']
+        },
+        {
+            name: 'Tate & Liza',
+            town: 'Mossdeep City',
+            specialty: 'Psychic',
+            badge: 'Mind Badge',
+            pokemonTeam: ['Solrock', 'Lunatone', 'Xatu']
+        },
+        {
+            name: 'Wallace',
+            town: 'Sootopolis City',
+            specialty: 'Water',
+            badge: 'Rain Badge',
+            pokemonTeam: ['Milotic', 'Whiscash', 'Gyarados']
         }
     ])
 
